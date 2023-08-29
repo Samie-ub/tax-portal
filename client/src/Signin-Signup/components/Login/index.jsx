004471cffc70a1b84dbcfab15eb5afccce7b52bc
Samie <78798205+Samie-ub@users.noreply.github.com>
2023-08-29 08:17:24 +0500

    Update index.jsx

--- client/src/Signin-Signup/components/Login/index.jsx
+++ client/src/Signin-Signup/components/Login/index.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "https://tax-portal-backend-fhsxt998n-samie-ub.vercel.app//api/auth";
+			const url = "https://tax-portal-backend-fhsxt998n-samie-ub.vercel.app/api/auth";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("user",res.data)
 			localStorage.setItem("token", res.data);
