7ad1a14253688fb441c585b838bd46687de1f607
Samie <78798205+Samie-ub@users.noreply.github.com>
2023-08-30 12:07:38 +0500

    Update Form.jsx

--- client/src/components/Dashboard/FormSide/Form.jsx
+++ client/src/components/Dashboard/FormSide/Form.jsx
@@ -8,18 +8,25 @@ function Form() {
 
   return (
     <div className="status_main">
-       <Grid sx={{ display: { xs: "block", md: "none" } }}>
+      <Grid container justifyContent={"center"} alignItems={"center"}>
+        <Grid item xs={12} lg={12}>
+        <Grid sx={{ display: { xs: "block", md: "none" } }}>
         <DashboardNav />
       </Grid>
-      <DashSidebar />
+     
       <div className="tax_form_main">
-        <Grid container justifyContent={"space-between"}>
-          <Grid item lg={1}></Grid>
-          <Grid item lg={9.5}>
+        <Grid container justifyContent={"space-between"} alignItems={"center"}>
+          <Grid item lg={1}>
+          <DashSidebar />
+          </Grid>
+          <Grid item xs={12} lg={9.5}>
             <Status formStep={formStep} setFormStep={setFormStep} />
           </Grid>
         </Grid>
       </div>
+        </Grid>
+      </Grid>
+      
     </div>
   );
 }
